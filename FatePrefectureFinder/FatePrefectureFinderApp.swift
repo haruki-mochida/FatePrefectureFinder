a32482d750dee9fbaeba274c78c10f8a2d457420
haruki-mochida <motimotimotida@gmail.com>
2023-12-14 21:34:25 +0900

    Initial Commit

---
+++ FatePrefectureFinder/FatePrefectureFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FatePrefectureFinderApp.swift
+//  FatePrefectureFinder
+//
+//  Created by 持田晴生 on 2023/12/14.
+//
+
+import SwiftUI
+
+@main
+struct FatePrefectureFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

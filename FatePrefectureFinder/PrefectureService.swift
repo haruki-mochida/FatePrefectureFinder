fcd8c1a0773d3e2b0ea67ee7518037077a205f61
haruki-mochida <motimotimotida@gmail.com>
2024-01-11 01:14:41 +0900

    レスポンスが失敗した場合、エラーロオ具を表示するように変更

--- FatePrefectureFinder/PrefectureService.swift
+++ FatePrefectureFinder/PrefectureService.swift
@@ -15,14 +15,21 @@ class PrefectureService {
     // API通信を行い、結果を取得する関数
     func fetchPrefecture(requestData: RequestData, completion: @escaping (Result<Prefecture, Error>) -> Void) {
         // Alamofireを使用してリクエストを送信
-        AF.request(url, method: .post, parameters: requestData, encoder: JSONParameterEncoder.default).responseDecodable(of: Prefecture.self) { response in
-            switch response.result {
-            case .success(let prefecture):
-                completion(.success(prefecture))
-            case .failure(let error):
-                completion(.failure(error))
+        AF.request(url, method: .post, parameters: requestData, encoder: JSONParameterEncoder.default)
+            .responseDecodable(of: Prefecture.self) { response in
+                // レスポンスの結果に基づいて処理を分岐
+                switch response.result {
+                case .success(let prefecture):
+                    // レスポンスが成功した場合、取得したデータをcompletionハンドラに渡す
+                    completion(.success(prefecture))
+                case .failure(let error):
+                    // レスポンスが失敗した場合、エラーログを表示し、エラーをcompletionハンドラに渡す
+                    if let data = response.data, let jsonString = String(data: data, encoding: .utf8) {
+                        print("Received JSON string: \(jsonString)")
+                    }
+                    completion(.failure(error))
+                }
             }
-        }
     }
 }
 
